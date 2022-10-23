bcb1cbc251609d7da49d3264717868216e7415d9
Vivek Pal <vivekpal@duck.com>
2022-10-23 15:47:32 +0000

    docss

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/workspace/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/workspace/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/workspace/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/workspace/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/workspace/docs/src/css/custom.css'),
-];
