eb747b2278111e86b11a8fc3dd0408375ba950f3
Vivek Pal <vivekpal@duck.com>
2022-11-06 01:55:35 +0000

    search

--- components/layout/nav/Logo.tsx
+++ components/layout/nav/Logo.tsx
@@ -1,33 +1,27 @@
-import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import corndocsConfig from "../../../corndocs.config";
+import corndocsConfig from "../../../corndocs.config.js";
 
 const customLoader = ({ src }: any) => {
   return src;
 };
 
 const Logo = () => {
-  const [fallbackImage, setFallbackImage] = useState(false);
-
+  const { logo } = corndocsConfig.project;
   return (
-    <Link href="/" passHref>
+    <Link href="/">
       <a className="flex h-full cursor-pointer items-center justify-start gap-2 text-xl font-black">
-        {!fallbackImage && (
-          <span className="relative h-10 w-24 overflow-hidden">
-            <Image
-              loader={customLoader}
-              src={`/static/logo.svg`}
-              alt="Profile Image"
-              width="100%"
-              height="100%"
-              layout="fill"
-              onError={() => {
-                setFallbackImage(true);
-              }}
-            />
-          </span>
-        )}
+        <span className="logo relative h-10 overflow-hidden">
+          <Image
+            loader={customLoader}
+            objectFit="contain"
+            objectPosition="center"
+            src={logo.src}
+            alt={logo.alt}
+            width={logo.size[0] ? logo.size[0] : 80}
+            height={logo.size[1] ? logo.size[1] : 40}
+          />
+        </span>
         <span className="hidden md:inline-block">
           {corndocsConfig.project.name}
         </span>
--- components/layout/nav/index.tsx
+++ components/layout/nav/index.tsx
@@ -2,39 +2,51 @@ import React from "react";
 import Logo from "./Logo";
 import DarkToggle from "./DarkToggle";
 import SidebarToggle from "./SidebarToggle";
-import corndocsConfig from "../../../corndocs.config";
+import corndocsConfig from "../../../corndocs.config.js";
+import Link from "next/link";
+import { Github } from "lucide-react";
 
 function Nav() {
   return (
-    <header className="sticky top-0 z-40 h-16 w-full flex-none border-b border-slate-300 bg-white/60 backdrop-blur-md dark:border-slate-800 dark:bg-slate-900/60 dark:text-white lg:z-50">
+    <header className="sticky top-0 z-40 h-16 w-full flex-none border-b border-slate-300 bg-white/90 backdrop-blur-md dark:border-slate-800 dark:bg-slate-900/90 dark:text-white lg:z-50">
       <div className="mx-auto h-full w-full max-w-[90rem]">
         <div className="mx-4 h-full lg:mx-0 lg:border-0 lg:px-8">
           <div className="relative flex h-full items-center">
-            <a
-              className="mr-3 w-24 flex-none overflow-hidden md:w-auto"
-              href="/"
-            >
+            <span className="mr-3 w-24 flex-none overflow-hidden md:w-auto">
               <span className="sr-only">{corndocsConfig.project.name}</span>
               <Logo />
-            </a>
+            </span>
             <div className="relative ml-auto hidden items-center lg:flex">
               <nav className="text-sm font-semibold leading-6">
-                <ul className="flex h-full items-center space-x-8">
+                <ul className="flex h-full items-center space-x-4">
                   <li>
-                    <a
-                      className="hover:text-primary-500 dark:hover:text-primary-400"
-                      href="/Docs/"
-                    >
-                      Docs
-                    </a>
+                    <Link href="/Docs/">
+                      <a className="block rounded-xl p-2 hover:bg-slate-200 hover:dark:bg-slate-800">
+                        Docs
+                      </a>
+                    </Link>
                   </li>
                 </ul>
               </nav>
-              <div className="ml-6 flex items-center border-l border-slate-300 pl-6 dark:border-slate-800">
+              <div className="ml-4 flex items-center space-x-2 border-l border-slate-300 pl-4 dark:border-slate-800">
+                <a
+                  className={`block rounded-xl p-2 hover:bg-slate-200 hover:dark:bg-slate-800`}
+                  href={corndocsConfig.project.github.repo}
+                  target={"_blank"}
+                >
+                  <Github size={20} />
+                </a>
                 <DarkToggle />
               </div>
             </div>
-            <div className="flex w-full justify-end gap-4 lg:hidden">
+            <div className="flex w-full justify-end lg:hidden">
+              <a
+                className={`block rounded-xl p-2 hover:bg-slate-200 hover:dark:bg-slate-800`}
+                href={corndocsConfig.project.github.repo}
+                target={"_blank"}
+              >
+                <Github size={20} />
+              </a>
               <DarkToggle />
               <SidebarToggle />
             </div>
